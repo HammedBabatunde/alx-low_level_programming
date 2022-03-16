769276dd5c3b85870bae9e9b4b5a1a51c818cf68
HammedBabatunde <olawale6708@gmail.com>
2022-03-16 23:02:02 +0100

    Updated tasks

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,11 +1,11 @@
 #include "main.h"
+
 /**
  * print_sign - Determines whether an integer is positive, negative or zero.
  * @n: the number to be checked.
  *
  * Return: 1 if positive, 0 if zero, -1 if negative, / if not a digit.
  */
-
 
 int print_sign(int n)
 {
@@ -14,12 +14,12 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n == 0) 
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
-	} 
-	else 
+	}
+	else
 	{
 		_putchar('-');
 		return (-1);
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,19 +7,18 @@
  * Return: last digit of number r
  */
 
-
 int print_last_digit(int r)
 {
 	int n;
 
-	if (r < 0) 
+	if (r < 0)
 	{
 		n = -1 * (r % 10);
 	}
-	else 
+	else
 	{
 		n = r % 10;
 	}
 	_putchar((n % 10) + '0');
-	return (n %10);
+	return (n % 10);
 }
