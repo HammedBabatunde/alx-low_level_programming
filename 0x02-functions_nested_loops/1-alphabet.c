4f92401f007d8af19d43aabd3e33281818fc9156
HammedBabatunde <olawale6708@gmail.com>
2022-03-16 02:12:43 +0100

    1-alphabet.c task updated

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,12 +6,12 @@
  * Return: Nothing.
  */
 
-void print_alphabet(void) 
+void print_alphabet(void)
 {
 	char c;
 
-	for (c = 'a'; c <= 'z'; c++) {
-		
+	for (c = 'a'; c <= 'z'; c++)
+	{
 		_putchar(c);
 	}
 	_putchar('\n');
