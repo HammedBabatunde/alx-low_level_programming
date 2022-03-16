f5055605f90a6c1a73fd8e991b46e558249c21b7
HammedBabatunde <olawale6708@gmail.com>
2022-03-16 01:24:08 +0100

    updated 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,12 +2,11 @@
 #include "main.h"
 
 /**
- *  * _putchar - writes the character c to stdout
- *   * @c: The character to print
- *    *
- *     * Return: On success 1.
- *      * On error, -1 is returned, and errno is set appropriately.
- *       */
+ * main - Check description
+ * Description: It prints the word _putchar, followed by a new line.
+ * Return: 0.
+ */
+
 int main(void)
 {
 	char word[9] = "_putchar\n";
